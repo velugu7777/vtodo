3b10dccd81a41a4ca126448bf09ce156c0f30d4f
velugu7777 <velugu.go@gmail.com>
2015-07-06 11:47:23 +0530

    update

--- app/models/todo.js
+++ app/models/todo.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name:DS.attr('string'),
-  isdone:DS.attr('boolean'),
+  isCompleted:DS.attr('boolean'),
 });
--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	//_ "github.com/lib/pq"
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 	"net/http"
 )
 
+var db *sql.DB
+
 type Todo struct {
-	Id     int    `json:"id"`
-	Name   string `json:"name"`
-	IsDone bool   `json:"isDone"`
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	IsCompleted bool   `json:"isCompleted"`
+}
+
+func init() {
+	var err error
+	db, err = sql.Open("postgres", "user=postgres password=Sunithak*1247 host=localhost dbname=postgres sslmode=disable")
+	if err != nil {
+		//		log.Fatal("Open connection failed:", err.Error())
+	}
 }
 
 type EmberTodo struct {
@@ -59,6 +71,18 @@ func newTodo(w http.ResponseWriter, req *http.Request) {
 }
 
 func putTodo(c web.C, w http.ResponseWriter, req *http.Request) {
+	var etodo EmberTodo
+	decoder := json.NewDecoder(req.Body)
+	err := decoder.Decode(&etodo)
+	if err != nil {
+		//		log.Println("JSON decode failed:", err.Error())
+	}
+	//	id, err := strconv.ParseInt(c.URLParams["id"], 10, 64)
+	if err != nil {
+		//		log.Println("URLParams[id] failed:", err.Error())
+	}
+	//	etodo.Todo.Id = int(id)
+	fmt.Println(etodo)
 
 }
 
